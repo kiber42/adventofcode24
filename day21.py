4fbb918bc0d0e1ddb0c2258a9092544f1125b2e3
Kilian Brendel <kilian.brendel@posteo.de>
2024-12-26 09:55:41 +0100

    WIP

--- day21.py
+++ day21.py
@@ -9,12 +9,33 @@ inputfile = "input{:02}.txt".format(day)
 
 # ------------------------------------------
 
+from itertools import pairwise
+
 def get_data(filename):
     return open(filename).read().splitlines()
 
 
+def complexity(code):
+    keypositions = {
+        "7": (0, 0), "8": (0, 1), "9": (0, 2),
+        "4": (1, 0), "5": (1, 1), "6": (1, 2),
+        "1": (2, 0), "2": (2, 1), "3": (2, 2),
+                     "0": (3, 1), "A": (3, 2)}
+    positions = [keypositions[key] for key in "A" + code]
+    # Distances on numeric keypad
+    steps0 = [(abs(y2 - y1), abs(x2 - x1)) for ((y1, x1), (y2, x2)) in pairwise(positions)]
+    # Distances on first arrow keypad
+    steps1 = []
+    # Distances on second arrow keypad
+    steps2 = []
+    # Keys to be pressed directly
+    sequence = []
+    numeric = int(code[:-1])
+    return numeric * len(sequence)
+
+
 def part_one(data):
-    return None
+    return sum(complexity(keycode) for keycode in data)
 
 
 def part_two(data):
